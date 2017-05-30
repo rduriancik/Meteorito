e3a01acbca8a25efc45d8c2a9217f42aaa9cd080
rduriancik <r.duriancik@gmail.com>
2017-05-30 16:39:57 +0200

    Use fragment_container in MainActivity

--- app/src/main/java/com/example/robertduriancik/meteorito/activities/MainActivity.java
+++ app/src/main/java/com/example/robertduriancik/meteorito/activities/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.robertduriancik.meteorito.activities;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.robertduriancik.meteorito.R;
+import com.example.robertduriancik.meteorito.fragments.MapFragment;
+import com.example.robertduriancik.meteorito.fragments.MeteoriteListFragment;
+import com.example.robertduriancik.meteorito.models.MeteoriteLanding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MeteoriteListFragment.OnMeteoriteListFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MeteoriteListFragment meteoriteListFragment = new MeteoriteListFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, meteoriteListFragment)
+                .commit();
+    }
+
+    @Override
+    public void onMeteoriteListItemClick(MeteoriteLanding meteoriteLanding) {
+        MapFragment mapFragment = MapFragment.newInstance(meteoriteLanding);
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragment_container, mapFragment);
+        transaction.addToBackStack(null);
+
+        transaction.commit();
     }
 }
