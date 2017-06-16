c2d75975fed68b4cc6df3d5ed34a1bf43c3ce091
rduriancik <r.duriancik@gmail.com>
2017-06-16 13:43:32 +0200

    Modify MainActivity to support multi-pane ui

--- app/src/main/java/com/example/robertduriancik/meteorito/activities/MainActivity.java
+++ app/src/main/java/com/example/robertduriancik/meteorito/activities/MainActivity.java
@@ -4,16 +4,22 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.FrameLayout;
 
 import com.example.robertduriancik.meteorito.R;
 import com.example.robertduriancik.meteorito.fragments.MapFragment;
 import com.example.robertduriancik.meteorito.fragments.MeteoriteListFragment;
 import com.example.robertduriancik.meteorito.models.MeteoriteLanding;
 
+import butterknife.BindView;
+
 public class MainActivity extends AppCompatActivity
         implements MeteoriteListFragment.OnMeteoriteListFragmentInteractionListener {
 
     private FragmentManager mFragmentManager;
+
+    @BindView(R.id.fragment_container_large)
+    FrameLayout mFragmentContainerLarge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,25 +28,32 @@ public class MainActivity extends AppCompatActivity
 
         mFragmentManager = getSupportFragmentManager();
 
-        // Protection against fragment overlapping
-        if (savedInstanceState != null) {
-            return;
-        }
+        if (mFragmentContainerLarge == null) {
+            // Protection against fragment overlapping
+            if (savedInstanceState != null) {
+                return;
+            }
 
-        MeteoriteListFragment meteoriteListFragment = new MeteoriteListFragment();
-        mFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, meteoriteListFragment)
-                .commit();
+            MeteoriteListFragment meteoriteListFragment = new MeteoriteListFragment();
+            mFragmentManager.beginTransaction()
+                    .add(R.id.fragment_container, meteoriteListFragment)
+                    .commit();
+        }
     }
 
     @Override
     public void onMeteoriteListItemClick(MeteoriteLanding meteoriteLanding) {
         MapFragment mapFragment = MapFragment.newInstance(meteoriteLanding);
-
         FragmentTransaction transaction = mFragmentManager.beginTransaction();
-        transaction.replace(R.id.fragment_container, mapFragment);
-        transaction.addToBackStack(null);
 
+        if (mFragmentContainerLarge == null) {
+            transaction.replace(R.id.fragment_container, mapFragment);
+
+        } else {
+            transaction.replace(R.id.fragment_container_large, mapFragment);
+        }
+
+        transaction.addToBackStack(null);
         transaction.commit();
     }
 }
