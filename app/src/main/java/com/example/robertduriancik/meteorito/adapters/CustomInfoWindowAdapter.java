0bf095286d7dbb8faa263463a4278f5845984e92
rduriancik <r.duriancik@gmail.com>
2017-06-19 12:05:43 +0200

    CustomInfoAdapter cleanup

--- app/src/main/java/com/example/robertduriancik/meteorito/adapters/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/example/robertduriancik/meteorito/adapters/CustomInfoWindowAdapter.java
@@ -1,22 +1,15 @@
 package com.example.robertduriancik.meteorito.adapters;
 
 import android.content.Context;
-import android.content.Intent;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.ResultReceiver;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import com.example.robertduriancik.meteorito.FetchAddressIntentService;
 import com.example.robertduriancik.meteorito.R;
 import com.example.robertduriancik.meteorito.models.MeteoriteLanding;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
+
+//import com.example.robertduriancik.meteorito.AddressResultReceiver;
 
 /**
  * Created by robert on 3.6.2017.
@@ -26,11 +19,9 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     private static final String TAG = "CustomInfoWindowAdapter";
 
     private Context mContext;
-    private ResultReceiver mReceiver;
 
     public CustomInfoWindowAdapter(Context context) {
         this.mContext = context;
-        mReceiver = new AddressResultReceiver(null);
     }
 
     @Override
@@ -49,48 +40,16 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
 
         MeteoriteLanding meteoriteLanding = (MeteoriteLanding) marker.getTag();
         if (meteoriteLanding != null) {
-            Location location = new Location("MeteoriteLocation");
-            location.setLatitude(meteoriteLanding.getLatitude());
-            location.setLongitude(meteoriteLanding.getLongitude());
-            fetchAddress(location);
-
-            // TODO get country name from coordinates
             // TODO design layout
 
             title.setText(marker.getTitle());
             meteorName.setText(meteoriteLanding.getName());
             meteorClass.setText(meteoriteLanding.getRecClass());
-
         } else {
             title.setText("No item");
         }
 
-
         return view;
     }
 
-    private void fetchAddress(Location location) {
-        if (location != null && Geocoder.isPresent()) {
-            Intent intent = new Intent(mContext, FetchAddressIntentService.class);
-            intent.putExtra(FetchAddressIntentService.Constants.RECEIVER, mReceiver);
-            intent.putExtra(FetchAddressIntentService.Constants.LOCATION_DATA, location);
-            mContext.startService(intent);
-        }
-    }
-
-    private class AddressResultReceiver extends ResultReceiver {
-        public AddressResultReceiver(Handler handler) {
-            super(handler);
-        }
-
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle resultData) {
-            if (resultCode == FetchAddressIntentService.Constants.SUCCESS_RESULT) {
-                final Address address = resultData.getParcelable(FetchAddressIntentService.Constants.RESULT_ADDRESS_KEY);
-                Log.d(TAG, "onReceiveResult: address" + address);
-            } else {
-                Log.d(TAG, "onReceiveResult: failed");
-            }
-        }
-    }
 }
