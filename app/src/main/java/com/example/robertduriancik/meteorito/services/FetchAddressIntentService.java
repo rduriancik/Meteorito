05fb5904173d9fa6277c9de3d920826388506564
rduriancik <r.duriancik@gmail.com>
2017-06-20 15:11:04 +0200

    Design update

--- app/src/main/java/com/example/robertduriancik/meteorito/services/FetchAddressIntentService.java
+++ app/src/main/java/com/example/robertduriancik/meteorito/services/FetchAddressIntentService.java
@@ -67,7 +67,7 @@ public class FetchAddressIntentService extends IntentService {
     public static final class Constants {
         public static final int SUCCESS_RESULT = 0;
         public static final int FAILURE_RESULT = 1;
-        public static final String PACKAGE_NAME = "com.example.robertduriancik.meteorito";
+        static final String PACKAGE_NAME = "com.example.robertduriancik.meteorito";
         public static final String LOCATION_DATA = PACKAGE_NAME + ".LOCATION_DATA";
         public static final String RECEIVER = PACKAGE_NAME + ".RECEIVER";
         public static final String RESULT_ADDRESS_KEY = PACKAGE_NAME + "RESULT_ADDRESS_KEY";
