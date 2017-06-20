77f22fde4888f05aac222277144cd0a8770947ec
rduriancik <r.duriancik@gmail.com>
2017-06-20 23:49:27 +0200

    CustomInfoWindowAdapter.java - bind() method

--- app/src/main/java/com/example/robertduriancik/meteorito/adapters/CustomInfoWindowAdapter.java
+++ app/src/main/java/com/example/robertduriancik/meteorito/adapters/CustomInfoWindowAdapter.java
@@ -9,6 +9,11 @@ import com.example.robertduriancik.meteorito.models.MeteoriteLanding;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
+import java.util.Calendar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 //import com.example.robertduriancik.meteorito.AddressResultReceiver;
 
 /**
@@ -19,6 +24,19 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     private static final String TAG = "CustomInfoWindowAdapter";
 
     private Context mContext;
+
+    @BindView(R.id.iw_meteorite_name)
+    TextView mMeteorName;
+    @BindView(R.id.iw_meteorite_name_type)
+    TextView mMeteorNameType;
+    @BindView(R.id.iw_meteorite_class)
+    TextView mMeteorClass;
+    @BindView(R.id.iw_meteorite_year)
+    TextView mMeteorYear;
+    @BindView(R.id.iw_meteorite_fall)
+    TextView mMeteorFall;
+    @BindView(R.id.iw_meteorite_mass)
+    TextView mMeteorMass;
 
     public CustomInfoWindowAdapter(Context context) {
         this.mContext = context;
@@ -32,24 +50,24 @@ public class CustomInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
     @Override
     public View getInfoContents(Marker marker) {
         View view = View.inflate(mContext, R.layout.info_window, null);
+        ButterKnife.bind(this, view);
 
-        TextView title = (TextView) view.findViewById(R.id.iw_title);
-        TextView meteorName = (TextView) view.findViewById(R.id.iw_meteorite_name);
-        TextView meteorClass = (TextView) view.findViewById(R.id.iw_meteorite_class);
-        TextView meteorAddress = (TextView) view.findViewById(R.id.iw_meteorite_address);
-
-        MeteoriteLanding meteoriteLanding = (MeteoriteLanding) marker.getTag();
-        if (meteoriteLanding != null) {
-            // TODO design layout
-
-            title.setText(marker.getTitle());
-            meteorName.setText(meteoriteLanding.getName());
-            meteorClass.setText(meteoriteLanding.getRecClass());
-        } else {
-            title.setText("No item");
-        }
+        bind((MeteoriteLanding) marker.getTag());
 
         return view;
+    }
+
+    private void bind(MeteoriteLanding landing) {
+        if (landing != null) {
+            mMeteorName.setText(landing.getName());
+            mMeteorNameType.setText(landing.getNameType());
+            mMeteorClass.setText(landing.getRecClass());
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(landing.getYear());
+            mMeteorYear.setText(String.valueOf(calendar.get(Calendar.YEAR)));
+            mMeteorFall.setText(landing.getFall());
+            mMeteorMass.setText(String.valueOf(landing.getMass()));
+        }
     }
 
 }
